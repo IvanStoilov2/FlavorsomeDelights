f4b2a09e428138d53c6b03e5f873c4b7804443d4
Ivan Stoilov <ivan_stoilov2@icloud.com>
2023-07-12 11:09:12 +0300

    push

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,5 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test 1 from SO");
 Console.WriteLine("Hi, I am Eli.");
-Console.ReadLine();
 Console.WriteLine("Hi, I'm Ivan. Nice to meet you!");
+Console.ReadLine();
+
