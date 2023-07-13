654bf3860a39294dfff260bd3b9ab17bb9d16984
Ivan Stoilov <ivan_stoilov2@icloud.com>
2023-07-13 16:43:19 +0300

    Ivan's end-of-the-day commit

---
+++ RecepieDelight/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecepieDelight.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
